2f6ceba695733ad0a2cd699840a07fcc3869c62a
nikoloz14 <nakhv12@yahoo.com>
2016-04-30 16:29:05 +0400

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Required for Devise, Remember to change localhost to actual app host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'etsydemo-nikoloz.herokuapp.com', port: 3000 }
 end
